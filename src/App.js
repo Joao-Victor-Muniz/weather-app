31e08ae403e9c04b55b11f3e97390527addd2b13
Joao Victor Muniz <screweed123@gmail.com>
2024-07-29 22:35:02 -0300

    new assets

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from ''
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <div className=''>
+
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
